d584c8852f075e0cd91d6fcf066832feee585a1b
Kevin Le <kevinle108@gmail.com>
2021-12-09 01:03:11 -0500

    cors error

--- src/routes/[searched]/index.svelte
+++ src/routes/[searched]/index.svelte
@@ -1,13 +1,20 @@
 <script>
+  import { page } from "$app/stores";
+  import { onMount } from "svelte";
 
+  const searched = $page.params.searched
 
-  
+  onMount(async () => {
+    const itunesSearched = await fetch(`https://itunes.apple.com/search?term=${searched}&entity=song`);
+    const res = await itunesSearched.json();
+    console.log(res)
+  });
+  // https://itunes.apple.com/search?term=nas&entity=song
 </script>
 
+<h1>You searched: {searched}</h1>
 <section>
-  <div>
-    <div></div>
-  </div>
+  <div></div>
 </section>
 
 <style>
